850ab4446e612a2b6e2fe65a21971037e6d4f6d6
miguelfernandezeys <miguel.fernandez@eys.co>
2017-09-14 14:56:37 -0500

    Update gefeli.sh

--- gefeli.sh
+++ gefeli.sh
@@ -1,12 +1,34 @@
 #!bin/sh
+#DECLARACIÓN DE VARIABLES
+FILEHOST=/etc/hosts
+
 # Instalar las actualizaciones del sistema
 
-yum -y update
+file="./nodos.properties"
 
-#Instalacion de repo MariaDB 10.0
-> /etc/yum.repos.d/MariaDB.repo
+if [ -f "$file" ];
 
-echo "
+then
+    echo "$file found"
+. $file
+else
+    echo "$file not found"
+fi
+k=0
+for i in "${hosts[@]}";
+do
+    echo ${hosts[k]}  ${dns[k]} >> /etc/hosts
+
+k=$k+1
+
+done
+
+k=0
+
+while [ $k -le ${#hosts[@]} ];do
+  scp -"$FILEHOST" root@${hosts[$k]}:/etc/
+  ssh root@${hosts[$k]} "yum -y update | firewall-cmd --zone=public --permanent --add-port=3306/tcp | firewall-cmd --reload | > /etc/yum.repos.d/MariaDB.repo"
+  ssh root@${hosts[$k]} 'echo "
 # MariaDB 10.0 CentOS repository list - created 2017-08-04 03:32 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
@@ -14,21 +36,35 @@ name = MariaDB
 baseurl = http://yum.mariadb.org/10.0/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
-" >> /etc/yum.repos.d/MariaDB.repo
-
-cat /etc/yum.repos.d/MariaDB.repo
+" >> /etc/yum.repos.d/MariaDB.repo'
+  ssh root@${hosts[$k]} "cat /etc/yum.repos.d/MariaDB.repo"
+  
+  ssh root@${hosts[$k]} "yum -y install MariaDB-server MariaDB-client rsync"
+  ssh root@${hosts[$k]} "systemctl enable mysql"
+  
+  #Configuración de demonio
+  ssh root@${hosts[$k]} "sed 's/\[mysqld]/[mysqld]\nserver-id=10\nlog-bin=mysql-bin/g' -i /etc/my.cnf.d/server.cnf"
+  ssh root@${hosts[$k]} "systemctl start mysql"
+  
+  #binlog y posición para replicación 
+  ssh root@${hosts[$k]} "bin=$(mysql -e 'show master status;' | tail -n 1 | awk {'print $1'})"
+  ssh root@${hosts[$k]} "pos=$(mysql -e "show master status;" | tail -n 1 | awk {'print $2'})"
+done
 
 sleep 4
 
-yum -y install MariaDB-server MariaDB-client rsync
+#sleep 1
 
-systemctl enable mysql
 
-#Configuración de demonio
 
-sed 's/\[mysqld]/[mysqld]\nserver-id=10\nlog-bin=mysql-bin/g' -i /etc/my.cnf.d/server.cnf
+echo $bin $pos
 
-systemctl start mysql
+#Exportar base de datos
+
+mysqldump mysql > mysql-db.sql
+
+rsync -Pazxvl mysql-db.sql root@192.168.56.102:/root/
+#
 
 #mysql_secure_installation configuración y creación de usuario de replicación
 mysql --user=root <<_EOF
@@ -41,13 +77,3 @@ GRANT REPLICATION SLAVE ON *.* TO 'reply'@'%' IDENTIFIED BY 'password';
 FLUSH TABLES WITH READ LOCK;
 FLUSH PRIVILEGES;
 _EOF
-
-sleep 1
-
-#binlog y posición para replicación 
-
-bin=$(mysql -u root -p1234567 -e 'show master status;' | tail -n 1 | awk {'print $1'})
-pos=$(mysql -u root -p1234567 -e "show master status;" | tail -n 1 | awk {'print $2'})
-
-echo $bin $pos
-
