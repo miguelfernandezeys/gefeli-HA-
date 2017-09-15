bbc0c5f11d2bc9cf1fa1c98a006258b06b7628e2
miguelfernandezeys <miguel.fernandez@eys.co>
2017-09-15 11:29:09 -0500

    new changes

--- gefeli.sh
+++ gefeli.sh
@@ -27,7 +27,8 @@ k=$k+1
 done
 
 k=0
-
+binMaster1 = 0
+posMaster1 = 0
 while [ $k -le ${#hosts[@]} ];do
 scp -"$FILEHOST" root@${hosts[$k]}:/etc/
 ssh root@${hosts[$k]} "sudo yum groupinstall 'Development Tools'"
@@ -48,38 +49,44 @@ gpgcheck = 1
 " >> /etc/yum.repos.d/MariaDB.repo'
 ssh root@${hosts[$k]} "cat /etc/yum.repos.d/MariaDB.repo"
   
-ssh root@${hosts[$k]} "yum -y install MariaDB-server MariaDB-client"
+ssh root@${hosts[$k]} "yum -y install MariaDB-server MariaDB-client rsync"
 ssh root@${hosts[$k]} "systemctl enable mariadb.service"
-ssh root@${hosts[$k]} "mysql_secure_installation"
+#ssh root@${hosts[$k]} "mysql_secure_installation"
 
-#Configuración de demonio
+if [  ${dns[k]} = "master1" ];
+then
 ssh root@${hosts[$k]} "sed 's/\[mysqld]/[mysqld]\nserver-id=10\nlog-bin=mysql-bin/g' -i /etc/my.cnf.d/server.cnf"
 ssh root@${hosts[$k]} "systemctl start mariadb.service"
-  
-#binlog y posición para replicación 
-bin=$(ssh root@${hosts[$k]} 'mysql -e "show master status;" | tail -n 1')
-arrayBin=$((echo $bin | awk {'print $1'}))
-#echo $arrayBin
-pos=$(ssh root@${hosts[$k]} "mysql -e 'show master status;' | tail -n 1")
-arrayPos=$((echo $pos | awk {'print $2'})
-#echo $arrayPos
+ssh root@{hosts[$k]} "mysql -e 'CREATE USER "reply"@"%" IDENTIFIED BY "reply"; GRANT REPLICATION SLAVE ON *.* TO "reply"@"%" IDENTIFIED BY "reply"; FLUSH PRIVILEGES; FLUSH TABLES WITH READ LOCK;'"
+ssh root@{hosts[$k]} "systemctl restart mariadb.service"
+#binlog y posición para replicación
+bin=$(ssh root@192.168.56.104 'mysql -e "show master status;" | tail -n 1')
+binMaster1=$(echo $bin | awk {'print $1'})
+#echo $bin
+pos=$(ssh root@192.168.56.104 "mysql -e 'show master status;' | tail -n 1")
+posMaster1=$(echo $pos | awk {'print $2'})
+#echo $pos
+ssh root@{hosts[$k]} "mysqldump mysql > mysql-db.sql"
+ssh root@{hosts[$k]} "rsync -Pazxvl mysql-db.sql root@{hosts[$k]}:/root/"
+ssh root@{hosts[$k]} "root@{hosts[$k]}:/root/"
+ssh root@{hosts[$k]} "mysql -e 'unlock tables; stop slave; change master to master_host= '{hosts[1]}' , master_user='reply', master_password='reply', master_log_file= 
+binMaster1 , master_log_pos=posMaster1; start slave; CREATE DATABASE glpi; CREATE USER "glpi"@"%" IDENTIFED BY "glpi"; GRANT ALL PRIVILEGES ON glpi TO "glpi"@"%" ; FLUSH PRIVILEGES; '"
+ssh root@{hosts[$k]} "glpi < base_de_datos.sql"
+ssh root@{hosts[$k]} "chmod +x gefeli.sh"
+ssh root@{hosts[$k]} "./gefeli.sh"
+else
+ssh root@${hosts[$k]} "sed 's/\[mysqld]/[mysqld]\nserver-id=20\nlog-bin=mysql-bin/g' -i /etc/my.cnf.d/server.cnf"
+ssh root@${hosts[$k]} "systemctl start mariadb.service"
+ssh root@{hosts[$k]} "mysql < mysql-db.sql"
+ssh root@{hosts[$k]} "mysql -e 'stop slave; change master to master_host= '{hosts[0]}' , master_user='reply', master_password='reply', master_log_file= 
+binMaster1 , master_log_pos=posMaster1; ; FLUSH PRIVILEGES; FLUSH TABLES WITH READ LOCK; start slave; show slave status'"
+
+fi
+
+let k = k + 1
 
 done
-
 sleep 4
-
-#sleep 1
-
-
-
-echo $bin $pos
-
-#Exportar base de datos
-
-mysqldump mysql > mysql-db.sql
-
-rsync -Pazxvl mysql-db.sql root@192.168.56.102:/root/
-#
 
 #mysql_secure_installation configuración y creación de usuario de replicación
 mysql --user=root <<_EOF
