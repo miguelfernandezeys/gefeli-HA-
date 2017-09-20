34391ff192e717671db2fab83a552f2fb24d8afc
miguelfernandezeys <miguel.fernandez@eys.co>
2017-09-20 13:42:14 -0500

    Update gefeli.sh

--- gefeli.sh
+++ gefeli.sh
@@ -39,16 +39,23 @@ echo "comienza configuracion del host: " ${hosts[$k]}
 #scp -"$FILEHOST" root@${hosts[$k]}:/etc/
 #echo "Copia del archivo " $FILEHOST "en el host: " ${hosts[$k]}
 ssh root@${hosts[$k]} "sudo yum -y  groupinstall 'Development Tools'"
+
 echo "herramientas de desarrollo instaladas"
 #ssh root@${hosts[$k]} "sed 's/SELINUX=enforcing/SELINUX=disabled/g' -i ~/etc/selinux/config"
 #echo "SELINUX configurado correctamente "
 ssh root@${hosts[$k]} "sudo yum -y update"
 echo "yum update ejecutado"
+
 ssh root@${hosts[$k]} "sudo firewall-cmd --zone=public --permanent --add-port=3306/tcp"
+
 ssh root@${hosts[$k]} "sudo firewall-cmd --reload"
+
 echo "firewall configurado y recargado"
+
 ssh root@${hosts[$k]} "> /etc/yum.repos.d/MariaDB.repo"
+
 echo "archivo repo creado"
+
 ssh root@${hosts[$k]} "echo '
 # MariaDB 10.1 CentOS repository list - created 2017-08-04 03:32 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
@@ -58,21 +65,21 @@ baseurl = http://yum.mariadb.org/10.1/centos7-amd64
 gpgkey = https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck = 1
 ' >> /etc/yum.repos.d/MariaDB.repo"
+
 ssh root@${hosts[$k]} "cat /etc/yum.repos.d/MariaDB.repo"
  
 ssh root@${hosts[$k]} "yum -y install MariaDB-server MariaDB-client rsync wget"
+
 echo "MariaDb Server y Cliente instalados"
-ssh root@${hosts[$k]} "systemctl enable mariadb.service"
-echo "servicio mariadb habilitado"
-#ssh root@${hosts[$k]} "mysql_secure_installation"
+
 let k=k+1	
 done
 
 k=0
 
 while [ $k -lt ${#hosts[@]} ];do
-if [  ${dns[$k]} = "master1" ];
-then
+if [  ${dns[$k]} = "master1" ]; then
+
 echo "INICIA LA CONFIGURACION DEL MASTER 1"
 
 sleep 3
@@ -80,6 +87,12 @@ sleep 3
 ssh root@${hosts[$k]} "sed 's/\[mysqld]/[mysqld]\nserver-id=10\nlog-bin=mysql-bin/g' -i /etc/my.cnf.d/server.cnf"
 
 echo "archivo server.cnf modificado"
+
+ssh root@${hosts[$k]} "systemctl enable mariadb.service"
+
+echo "servicio mariadb habilitado"
+#ssh root@${hosts[$k]} "mysql_secure_installation"
+
 
 sleep 3
 
@@ -101,15 +114,20 @@ echo "Usuario reply creado"
 sleep 3
 
 ssh root@${hosts[$k]} "systemctl restart mariadb.service"
+
 echo "Reinicio del servicio MariaDB"
 
 sleep 2
 
 #binlog y posición para replicación
 bin=$(ssh root@${hosts[1]} 'mysql -e "show master status;" | tail -n 1')
+
 binMaster1=$(echo $bin | awk {'print $1'})
+
 pos=$(ssh root@${hosts[1]} "mysql -e 'show master status;' | tail -n 1")
+
 posMaster1=$(echo $pos | awk {'print $2'})
+
 echo "Variables POS: " $posMaster1 "y BIN: " $binMaster1
 
 sleep 2
@@ -140,16 +158,35 @@ sleep 2
 
 else
 echo "INICIA LA CONFIGURACION DEL MASTER 2"
+
 scp /etc/hosts root@${hosts[$k]}:/etc/
+
 echo "Copia del archivo " $FILEHOST "en el host: " ${hosts[$k]}
+
 scp mysql-db.sql root@${hosts[$k]}:/root/
+
 scp glpi.sql root@${hosts[$k]}:/root/
+
 scp scriptgefeli.sh root@${hosts[$k]}:/root/
+
 ssh root@${hosts[$k]} "sed 's/\[mysqld]/[mysqld]\nserver-id=20\nlog-bin=mysql-bin/g' -i /etc/my.cnf.d/server.cnf"
 
 echo "archivo server.cnf configurado"
 
 sleep 2
+
+ssh root@${hosts[$k]} "systemctl enable mariadb.service"
+
+echo "servicio mariadb habilitado"
+#ssh root@${hosts[$k]} "mysql_secure_installation"
+
+sleep 3
+
+ssh root@${hosts[$k]} "systemctl start mariadb.service"
+
+echo "inicia el servicio de MariaDB"
+
+
 
 ssh root@${hosts[$k]} "systemctl start mariadb.service"
 
@@ -172,9 +209,6 @@ posMaster1=$(echo $pos | awk {'print $2'})
 echo "Variables POS: " $posMaster1 "y BIN: " $binMaster1
 
 sleep 2
-
-
-
 
 ssh root@${hosts[$k]} "
 mysql --user=root <<_EOF
@@ -199,6 +233,7 @@ echo "base de datos restaurada"
 
 
 fi
+
 let k=k+1
 done
 
